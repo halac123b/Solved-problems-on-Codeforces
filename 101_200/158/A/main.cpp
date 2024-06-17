88de1bcc6a26e1b1620a5f3882785f9ce71f7674
halac123b <halac123b@gmail.com>
2024-06-18 02:09:20 +0700

    Update 17/6/24, currently working on 1 more

---
+++ 101_200/158/A/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Số người tham gia
+  int numberPeople;
+  // Index của thành viên để so sánh
+  int checkIndex;
+
+  cin >> numberPeople >> checkIndex;
+  int peopleArray[numberPeople];
+  for (int i = 0; i < numberPeople; i++)
+  {
+    cin >>
+  }
+}
